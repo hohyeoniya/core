66a33655052e5b3ce3dae787e39a29e25caf18af
Hyeoniya <h.hyeon0601@gmail.com>
2024-12-02 16:07:46 +0900

    Day10- variable DI (Test Edit)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -29,9 +29,9 @@ public class AutoAppConfig {
         return new OrderServiceImpl(memberRepository, discountPolicy);
     }*/
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 
 }
