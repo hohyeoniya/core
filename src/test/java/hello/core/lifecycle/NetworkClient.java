43891141eb04228b22d6f7fc27252d31e1023399
Hyeoniya <h.hyeon0601@gmail.com>
2024-12-12 20:16:15 +0900

    Day12_3- @PostConstruct, @PreDestroy

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
     /* 스프링 빈 라이프사이클 : 객체생성 -> 의존관계 주입(setter, 필드주입) (생성자 주입은 예외)*/
     /* 스프링 빈의 이벤트 라이프사이클(싱글톤)
@@ -29,16 +32,20 @@ public class NetworkClient {
     }
 
     /*
-    * 메서드 이름을 자유롭게 줄 수 있다.
-    * 스프링 빈이 스프링 코드에 의존하지 않는다.
-    * 코드가 아니라 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있다.
+    * @PostConstruct, @PreDestroy 애노테이션 특징
+    * 최신 스프링에서 가장 권장하는 방법이다.
+    * 어노테이션 하나만 붙이면 되므로 매우 편리하다.
+    * 유일한 단점!! 외부 라이브러리에는 적용하지 못한다는 것이다.
+    * 코드를 고칠 수 없는 외부 라이브러리를 초기화, 종료해야 하면 @Bean의 initMethod, destroyMethod를 사용하자.
     */
+    @PostConstruct
     public void init(){
         System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
+    @PreDestroy
     public void close(){
         System.out.println("NetworkClient.close");
         disconnect();
