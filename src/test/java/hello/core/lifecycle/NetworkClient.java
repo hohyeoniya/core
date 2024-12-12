a8384f70b79ddf6c0413438115ebcf5084be3f37
Hyeoniya <h.hyeon0601@gmail.com>
2024-12-12 20:07:00 +0900

    Day12_2- @Bean(initMethod, destroyMethod)

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,9 +1,6 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     /* 스프링 빈 라이프사이클 : 객체생성 -> 의존관계 주입(setter, 필드주입) (생성자 주입은 예외)*/
     /* 스프링 빈의 이벤트 라이프사이클(싱글톤)
     * :스프링 컨테이너 생성 -> 스프링 빈 생성 -> 의존관계 주입 -> 초기화 콜백 -> 사용 -> 소멸전 콜백 -> 스프링 종료 */
@@ -31,16 +28,19 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override/*의존관계 주입이 끝나면*/
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("NetworkClient.afterPropertiesSet");
+    /*
+    * 메서드 이름을 자유롭게 줄 수 있다.
+    * 스프링 빈이 스프링 코드에 의존하지 않는다.
+    * 코드가 아니라 설정 정보를 사용하기 때문에 코드를 고칠 수 없는 외부 라이브러리에도 초기화, 종료 메서드를 적용할 수 있다.
+    */
+    public void init(){
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    @Override
-    public void destroy() throws Exception {
-        System.out.println("NetworkClient.destroy");
+    public void close(){
+        System.out.println("NetworkClient.close");
         disconnect();
     }
 }
